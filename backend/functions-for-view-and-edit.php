3dc05aa92e6231ef7e9823de67d51957d149b0af
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-01-05 18:44:10 -0500

    functions for view note

--- backend/functions-for-view-and-edit.php
+++ backend/functions-for-view-and-edit.php
@@ -24,7 +24,7 @@ function createEntryTable($js) {
 }
 
 function createEntryList($js) {
-    echo "<ul class=\"in-section-p\">";
+    echo "<ul class=\"entry_list\">";
     foreach ($js->{'content'} as $item) {
         echo "<li class = \"entry-list-item\">".$item."</li>";
     }
@@ -60,6 +60,78 @@ function createEntrySection($js, $title) {
                 createEntryParagraph($element);
                 break;
         }
+    }
+    echo "</div></div>";
+}
+
+function createNoteTable($js) {
+    echo "<table class = \"note-table\">";
+    echo "<thead><tr class = \"note-table-head\">";
+    $heads = $js->{'head'};
+    foreach ($heads as $value) {
+        echo "<th><p class = \"note-table-header\">".$value."</p>";
+    }
+    echo "</tr></thead><tbody>";
+    $bodies = $js->{'table'};
+    foreach ($bodies as $row) {
+        echo "<tr class = \"note-table-body\">";
+        foreach ($heads as $value) {
+            echo "<td><p class = \"note-table-cells\">";
+            if (isset($row->{$value})) {
+                echo $row->{$value};
+            } else {
+                echo "";
+            }
+            echo "</p></td>";
+        }
+        echo "</tr>";
+    }
+}
+
+function createNoteList($js) {
+    echo "<ul class=\"note-list\">";
+    foreach ($js->{'content'} as $item) {
+        echo "<li class = \"note-list-item\">".$item."</li>";
+    }
+    echo "</ul>";
+}
+
+function createNoteParagraph($js) {
+    echo "<p class = \"note-content-p\">".$js->{'content'}."</p>";
+}
+
+function createNoteMatrix($js) {
+    echo "<div class = \"note-matrix\"><div class = \"matrix-neg-x-axis\">";
+    echo $js->{'x'}->{'negative'};
+    echo "</div><table class = \"matrix-mid\"><tr><td class = \"matrix-pos-y-axis\" colspan=\"2\">";
+    echo $js->{'y'}->{'positive'};
+    echo "</td></tr><td class = \"matrix-left-top\">";
+    echo $js->{'content'}->{'-x+y'};
+    echo "</td><td class = \"matrix-right-top\">";
+    echo $js->{'content'}->{'+x+y'};
+    echo "</td></tr><tr><td class = \"matrix-right-bottom\">";
+    echo $js->{'content'}->{'-x-y'};
+    echo "</td><td class = \"matrix-right-bottom\">";
+    echo $js->{'content'}->{'+x-y'};
+    echo "</td></tr><tr><td class = \"matrix-neg-y-axis\" colspan = \"2\">";
+    echo $js->{'y'}->{'negative'};
+    echo "</td></tr></table><div class = \"matrix-pos-x-axis\">";
+    echo $js->{'x'}->{'positive'};
+    echo "</div></div>";
+}
+
+function createNoteProcess($js) {
+    echo "<div class = \"note-process\"><dic class = \"note-process-main-container\">";
+    $l = 0;
+    foreach ($js->{'content'} as $item) {
+        if ($l != 0) {
+            echo "<img src = \"../image/note-process-arrow.png\" height = \"25\" class = \"note-process-arrow\">";
+
+        }
+        echo "<div class = \"note-process-single-process\">";
+        echo $item;
+        echo "</div>";
+        $l++;
     }
     echo "</div></div>";
 }
--- pages/view/note-view.php
+++ pages/view/note-view.php
@@ -1,9 +1,72 @@
 <html>
     <head>
-
+        <link rel="stylesheet" type="text/css" href="../../css/component.css">
+        <link rel="stylesheet" type="text/css" href="../../css/create.css">
     </head>
     <body>
-
-    </body>
-
+    <?php include_once '../../component/page-top.php'; ?>
+    <main class="create-main" id="note-create-main">
+        <hr/>
+        <?php include_once 'common-view.php'; ?>
+        <h3>笔记-Note</h3>
+        <table class="note-root-table">
+            <tr>
+                <td class="note-root-left-column">
+                    Cues
+                </td>
+                <td>
+                    笔记内容 - Content
+                </td>
+            </tr>
+            <tr>
+                <td class="note-root-left-column">
+                    <div class="cue-container">
+                        <?php
+                        $statement = "select note_id, note, summary from notes where id = ".$id;
+                        $outcome = mysqli_query($mysqli, $statement);
+                        $row = mysqli_fetch_row($outcome);
+                        $note_id = $row[0];
+                        $content_directory = "../".$row[1];
+                        $summary_directory = "../".$row[2];
+                        $statement = "select cue_id from cue_ref where note_id = ".$note_id;
+                        $outcome = mysqli_query($mysqli, $statement);
+                        $row = mysqli_fetch_lengths($outcome);
+                        while ($row != null) {
+                            $cue_id = $row[0];
+                            $statement = "select cue from cues where cue_id = ".$cue_id;
+                            $outcome1 = mysqli_query($mysqli, $statement);
+                            $cue = mysqli_fetch_row($outcome1)[0];
+                            echo "<p class=\"cue-content\">&#8226".$cue."</p>";
+                            $row = mysqli_fetch_lengths($outcome);
+                        }
+                        ?>
+                    </div>
+                </td>
+                <td>
+                    <div class="note-container">
+                        <p class="note-content-p" contenteditable="true">
+                            请在此添加内容 Please Fill Content Here
+                        </p>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <div class="summary-container">
+            <h3>总结-Summary</h3>
+            <div class="note-summary" contenteditable="true">
+                总结 Summary
+            </div>
+        </div>
+        <div class="note-comment">
+            <h3>评论-Comments</h3>
+            <div class="note-comment-content" contenteditable="true">
+                请在此添加新的评论 Comment Here
+            </div>
+            <button class="add-button" id="note-comment-add" onclick="addNoteComment()">添加 Add</button>
+        </div>
+        <button class="submit" onclick="createNote()">提交 Submit</button>
+    </main>
+    <script src="../../javascript/note-create-js.js"></script>
+    <script src="../../javascript/create-submit-js.js"></script>
+    </body
 </html>
